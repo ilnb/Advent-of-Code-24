c003a857a771c5f3dd167c0fcdb54234e22a9d8c
ilnb <lnbsa12345@gmail.com>
2024-12-10 12:48:45 -0800

    Day 5

---
+++ Day 5/aoc5.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define RULES 1176
+#define UPDATES 194
+
+typedef struct rule {
+  int main;
+  int sub;
+} rule;
+
+int checkUpdate(rule *, int *, int);
+void reverse(int *, int);
+void fixer(rule *, int *, int);
+
+int main(void) {
+  rule *rules = malloc(RULES * sizeof(rule));
+  FILE *fp = fopen("input.txt", "r");
+  int ordered = 0, rest = 0;
+  // rule parsing
+  for (int i = 0; i < RULES; i++) {
+    fscanf(fp, "%d|%d\n", &rules[i].main, &rules[i].sub);
+  }
+  // update parsing
+  for (int i = 0; i < UPDATES; i++) {
+    int count = 0, offset = 0;
+    char c;
+    while ((c = fgetc(fp)) != '\n') {
+      if (c == ',') {
+        count++;
+      }
+      offset++;
+    }
+    count++, offset++;
+    int *updates = malloc(sizeof(int) * count);
+    fseek(fp, -offset, SEEK_CUR);
+    for (int j = 0; j < count - 1; j++) {
+      fscanf(fp, "%d,", &updates[j]);
+    }
+    fscanf(fp, "%d\n", &updates[count - 1]);
+    int *kek = NULL, p = 0;
+    // valid updates
+    int flag = checkUpdate(rules, updates, count);
+    if (flag == 1) {
+      ordered += updates[count / 2];
+    } else {
+      p = count;
+      kek = malloc(sizeof(int) * p);
+      for (int j = 0; j < p; j++) {
+        kek[j] = updates[j];
+      }
+    }
+    // invalid ones
+    if (p > 0) {
+      reverse(kek, p);
+      fixer(rules, kek, p);
+      rest += kek[p / 2];
+    }
+    free(kek);
+    free(updates);
+  }
+  free(rules);
+  fclose(fp);
+  printf("ordered: %d\nrest: %d\n", ordered, rest);
+  return 0;
+}
+
+int checkUpdate(rule *rules, int *updates, int count) {
+  int flag = 1;
+  for (int j = 0; j < count; j++) {
+    for (int k = 0; k < count; k++) {
+      int m = updates[j];
+      int n = updates[k];
+      for (int l = 0; l < RULES; l++) {
+        if (k > j) {
+          if (m == rules[l].sub && n == rules[l].main) {
+            flag--;
+          }
+        } else if (k < j) {
+          if (m == rules[l].main && n == rules[l].sub) {
+            flag--;
+          }
+        }
+      }
+    }
+  }
+  return flag;
+}
+
+void reverse(int *arr, int n) {
+  for (int j = n - 1; j >= 1; j--) {
+    for (int i = 0; i < j; i++) {
+      if (arr[i] > arr[i + 1]) {
+        int t = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = t;
+      }
+    }
+  }
+}
+
+void fixer(rule *rules, int *kek, int count) {
+  for (int j = 0; j < count; j++) {
+    for (int k = 0; k < count; k++) {
+      for (int l = 0; l < RULES; l++) {
+        if (k > j) {
+          if (kek[j] == rules[l].sub && kek[k] == rules[l].main) {
+            int t = kek[j];
+            kek[j] = kek[k];
+            kek[k] = t;
+          }
+        } else if (k < j) {
+          if (kek[j] == rules[l].main && kek[k] == rules[l].sub) {
+            int t = kek[j];
+            kek[j] = kek[k];
+            kek[k] = t;
+          }
+        }
+      }
+    }
+  }
+}
